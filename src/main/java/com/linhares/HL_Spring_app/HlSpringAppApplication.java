5295aee987311c8a37c790639bd0fb84614ba532
Hian Linhares <hianfernando@hotmail.com>
2025-06-14 17:47:30 -0300

    first Commit - HL Spring boot

---
+++ src/main/java/com/linhares/HL_Spring_app/HlSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.linhares.HL_Spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linhares/HL_Spring_app/HlSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhares.HL_Spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
